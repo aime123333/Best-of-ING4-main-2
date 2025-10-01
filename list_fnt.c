665ba389284491aaea8e67f970d78e55d9fd1c1d
marcdeyed <martindeyed@gmail.com>
2025-10-01 12:41:23 +0200

    TESTEST

--- list_fnt.c
+++ list_fnt.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-
-#define MAX_LEN 50
-#define NB_USERS 5
-
 
 // Fonction de saisie (groupe 1)
 void demander_identifiants(char *identifiant, char *motdepasse) {
